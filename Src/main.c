414d6aee041844d6cae42b6ea8a7ed20240e55c0
WangLumeng <wanglm@sinoagg.com>
2018-07-18 16:45:09 +0800

    changed #define switches' name

--- Src/main.c
+++ Src/main.c
@@ -59,28 +59,43 @@
 #include "cmd.h"
 #include "EMRR.h"
 
+/* Switches ------------------------------------------------------------------*/
+/**
+ * ARS408
+ * EMRR
+*/
+#define RADAR_TYPE EMRR
+#define DBC_SEND 0
+#define ADAS_COMM 1
+//	labview
+#define RADAR_DATA_SEND 0
+//	Vehicle Speed & gyro via CAN3
+#define CAN_READ_VEHICLE 0
+/**
+ * BYD
+ * YUTONG
+ * KINGLONG
+*/
+#define VEHICLE_MODEL YUTONG
+
+/* Defines -------------------------------------------------------------------*/
 #define MAX_OBJ_NUM	4										//最大目标识别数量
 #define LANEWIDTH 1.5f									//车道线宽度
 #define MAX_DECELARATION 0.4*9.8f				//制动系统最大减速度
 #define DELAY_TIME	0.4f								//系统延迟时间
 #define LIMIT_RANGE 200									//计算碰撞时间的极限距离/m
-#define VEHICLE_SPEED_ADDR_HIGH 0x18FE
-#define VEHICLE_SPEED_ADDR_LOW	0x6E0B
+#if VEHICLE_MODEL == YUTONG
+	#define VEHICLE_SPEED_ADDR_HIGH 0x18FE
+	#define VEHICLE_SPEED_ADDR_LOW	0x6E0B
+#elif VEHICLE_MODEL == KINGLONG
+	#define VEHICLE_SPEED_ADDR_HIGH 0x18FE
+	#define VEHICLE_SPEED_ADDR_LOW	0x6E0B
+#else	//BYD
+	#define VEHICLE_SPEED_ADDR_HIGH 0x18FE
+	#define VEHICLE_SPEED_ADDR_LOW	0xF100
+#endif
 #define DBC_ADDR 0x509
 
-//switches
-/**
- * RADAR_TYPE
- * 0  EMRR
- * 1  ARS408
-*/
-#define RADAR_TYPE 0
-#define DBC_SEND 0
-#define ADAS_COMM 1
-//labview
-#define RADAR_DATA_SEND 0
-//Vehicle Speed & gyro via CAN3
-#define CAN_READ_VEHICLE 0
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
@@ -361,10 +376,10 @@ int main(void)
   //hcan1~hcan3 init, start
 	DBC_Init(&hcan1);
 	//ARS408
-	#if RADAR_TYPE
+	#if RADAR_TYPE == ARS408
   ARS_Init(&hcan2);//hcan2 must use hcan1
 	//EMRR
-	#else
+	#elif RADAR_TYPE == EMRR
 	EMRR_Init(&hcan2);
 	#endif
   #if CAN_READ_VEHICLE
@@ -765,10 +780,10 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
   	HAL_CAN_GetRxMessage(&hcan2, CAN_FILTER_FIFO0, &RadarCANRxHeader, RadarCANRxBuf);
   	osSemaphoreRelease(bSemRadarCANRxSigHandle);
 		//ARS408
-		#if RADAR_TYPE
+		#if RADAR_TYPE == ARS408
 		
 		//EMRR
-		#else
+		#elif RADAR_TYPE == EMRR
 		EMRR_RadarRxComplete = 1;
 		#endif
   	//__HAL_CAN_CLEAR_FLAG(hcan, CAN_FLAG_FF0);
@@ -782,8 +797,18 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
     if(0x18FF0DE6 == VehicleCANRxHeader.ExtId)      //gyroscope ID
       //start gyro semaphore
       osSemaphoreRelease(bSemGyroCommSigHandle);
+      
+      #if VEHICLE_MODEL == BYD
+    else if(0x18FEF100 == VehicleCANRxHeader.ExtId) //VehicleSpeed ID
+      osSemaphoreRelease(bSemSpeedRxSigHandle);
+      #elif VEHICLE_MODEL == YUTONG
     else if(0x18FE6E0B == VehicleCANRxHeader.ExtId) //VehicleSpeed ID
-  	  osSemaphoreRelease(bSemSpeedRxSigHandle);
+      osSemaphoreRelease(bSemSpeedRxSigHandle);
+      #elif VEHICLE_MODEL == KINGLONG
+    else if(0x18FE6E0B == VehicleCANRxHeader.ExtId) //VehicleSpeed ID
+      osSemaphoreRelease(bSemSpeedRxSigHandle);
+      #endif
+
     #endif
 	}
 	
@@ -873,7 +898,7 @@ void StartRadarCommTask(void const * argument)
 {
   /* USER CODE BEGIN StartRadarCommTask */
 	//ARS408
-	#if RADAR_TYPE
+	#if RADAR_TYPE == ARS408
 	uint8_t minRadarDistFlag = 0;
 	#endif
   /* Infinite loop */
@@ -882,7 +907,7 @@ void StartRadarCommTask(void const * argument)
 		osSemaphoreWait(bSemRadarCANRxSigHandle, osWaitForever);
 		HAL_GPIO_TogglePin(LED1_GPIO_Port,LED1_Pin);
     //ARS408
-		#if RADAR_TYPE
+		#if RADAR_TYPE == ARS408
 		if(RadarCANRxHeader.StdId==0x60A)												//60B的之前都读取完毕，开始计算
 		{
 			minRadarDistFlag = 1;
@@ -898,7 +923,7 @@ void StartRadarCommTask(void const * argument)
 		}
 		
     //EMRR
-		#else
+		#elif RADAR_TYPE == EMRR
 		EMRR_GetRaderObjCloset(RadarCANRxBuf, aEMRRGeneral, &EMRRGeneral_Closet);
 		if(EMRRGeneral_Closet.trackRange != 0)
 			osSemaphoreRelease(bSemCalculateSigHandle);
@@ -1111,10 +1136,10 @@ void StartCANSpeedReadTask(void const * argument)
     {
       VehicleSpeed = VehicleCANRxBuf[1];	//车速16进制,km/h
 			//ARS408
-			#if RADAR_TYPE
+			#if RADAR_TYPE == ARS408
 			ARS_SendVehicleSpeed(&hcan2, VehicleSpeed);	//send VehicleSpeed to Radar
 			//EMRR
-			#else
+			#elif RADAR_TYPE == EMRR
 			#endif
     }
 		osDelay(10);
@@ -1128,7 +1153,7 @@ void StartCalculateTask(void const * argument)
 	{
 		osSemaphoreWait(bSemCalculateSigHandle, osWaitForever);
     //ARS408
-		#if RADAR_TYPE
+		#if RADAR_TYPE == ARS408
 		uint16_t MinRange=255;									//初始化为最大距离
 		uint32_t relSpeed=0;
 		MinRange = RadarGeneral[0].Obj_DistLong;
@@ -1152,7 +1177,7 @@ void StartCalculateTask(void const * argument)
 		}
 
     //EMRR
-		#else
+		#elif RADAR_TYPE == EMRR
 		MinRangeLong = EMRRGeneral_Closet.trackRange;
     VrelLong = EMRRGeneral_Closet.trackSpeed;
 
